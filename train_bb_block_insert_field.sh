4ff03fdcc2e378fa17b03ca0d8a4796185dafdbc
kifarid <faridk@informatik.uni-freiburg.de>
2022-12-16 12:38:51 +0100

    adding new features

--- train_bb_block_insert_field.sh
+++ train_bb_block_insert_field.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=alldlc_gpu-rtx2080 # partition
-#SBATCH --array=1-3
+#SBATCH --array=1 #-3
 #SBATCH --mem=8Gb # RAM memory pool for each core (4GB)
 #SBATCH -t 0-8:00 # time (D-HH:MM)
 #SBATCH -c 8 # number of CPU cores you need
@@ -14,7 +14,7 @@ echo "Started at $(date)";
 echo "Running job $SLURM_JOB_NAME and $SLURM_ARRAY_TASK_ID using $SLURM_JOB_CPUS_PER_NODE cpus per node with given JID $SLURM_JOB_ID on queue $SLURM_JOB_PARTITION";
 
 
-export EXP_NAME=block_insert_field_mask_more'_'$SLURM_ARRAY_TASK_ID
+export EXP_NAME=block_insert_field_mask_4'_'$SLURM_ARRAY_TASK_ID
 python3.8 -m main -n $EXP_NAME \
 --base configs/default_mv_lgn.yaml configs/exp/ravens_global_field.yaml \
 -t \
