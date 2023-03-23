fixed error in normalization

src/model/encoder.py
@@ -126,8 +126,8 @@ class SpatialEncoder(nn.Module):
         #check if backbone is resnet
         if 'resnet' in self.backbone:
             # normalize image
-            print("normalizing image for resnet")
-            mean, std = torch.tensor([0.485, 0.456, 0.406]), torch.tensor([0.229, 0.224, 0.225])
+            mean = torch.tensor([0.485, 0.456, 0.406]).to(x.device)
+            std = torch.tensor([0.229, 0.224, 0.225]).to(x.device)
             x = (x - mean[None, :, None, None]) / std[None, :, None, None]
 
         if self.feature_scale != 1.0: